cd46dbb2d1f9387935b04bed9469053699b9af56
Simple-ux <kirill_99-99@mail.ru>
2022-11-02 19:13:27 +0300

    2.2 modified

--- 2.postgre.sql
+++ 2.postgre.sql
@@ -1,20 +1,27 @@
 -- 2.1 --
-select rep.user_id, 
-(
-select  sum(rep.reward)
-from reports rep 
-where rep.created_at between  '2022-01-01' and  '2022-12-31'
-)
-
-from reports rep
-where rep.created_at between  '2021-01-01' and  '2022-01-01';
-
+select
+    rep.user_id,
+    (
+        select
+            sum(rep.reward)
+        from
+            reports rep
+        where
+            rep.created_at between '2022-01-01'
+            and '2022-12-31'
+    )
+from
+    reports rep
+where
+    rep.created_at between '2021-01-01'
+    and '2022-01-01';
 
 -- 2.2 --
-
-SELECT rep.barcode, rep.price, pos.title  
-FROM reports rep
-left join pos
-on rep.pos_id = pos.id;  
-
--- Не понятно что необходимо сделать во второй задаче по условию, поэтому сделал просто join
+select
+    array_agg(rep.barcode),
+    array_agg(rep.barcode)
+FROM
+    reports rep
+    left join pos on rep.pos_id = pos.id
+group by
+    pos.title
