af9cd59a2950bf06677cafc7b4b484702b8103b4
Kirill Karasev <61629596+Simple-ux@users.noreply.github.com>
2022-12-04 17:27:06 +0300

    Create code.py

---
+++ code.py
@@ -0,0 +1,11 @@
+1. def to_camel_case(text): 
+       return re.split('_|-', text)[0].title() + ''.join(word.title() for word in re.split('_|-', text)[1::]) 
+  
+2. class SingletonMeta(type):
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
