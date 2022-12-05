4757acacef033d38554dd0370c5dfa32dcb8bf8a
Kirill Karasev <61629596+Simple-ux@users.noreply.github.com>
2022-12-05 15:32:32 +0300

    Update code.py

--- code.py
+++ code.py
@@ -5,7 +5,7 @@
 
     _instances = {}
 
-    def __call__(cls, *args, **kwargs):
+    def str(cls, *args, **kwargs):
         if cls not in cls._instances:
             cls._instances[cls] = super(SingletonMeta, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
